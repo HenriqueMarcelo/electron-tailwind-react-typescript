fdcdc727e2091f683e16a843b74a9e5ef1a0cf77
Marcelo Henrique Fonseca <marcelohenriquedasilvafonseca@gmail.com>
2024-06-27 14:33:33 -0300

    feat: using react

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,5 @@
+export function Home() {
+    return (
+        <h1>Olá Mundo!</h1>
+    )
+}
---
+++ src/app.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from 'react-dom/client';
+import { Home } from './Pages/Home';
+
+const root = createRoot(document.body);
+root.render(<Home/>);
